dd988e748f7a0aaf161164f284fa25f237e21f7a
Sanjay <sanjaymunda0123@gmail.com>
2023-06-09 22:32:28 +0530

    Product Listing

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return view('product');
+        $data = Product::all();
+        // echo "<pre>";
+        // print_r($data);
+        // echo "</pre>";
+
+        return view('product',['products'=>$data]);
     }
 }
